d2f50c897f37927440472e5a95f491803c668f06
Magnus Teschner <magnus.2002.mt@gmail.com>
2023-11-18 17:04:07 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return +((5 / 9) * (fahrenheit - 32)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return +((9 / 5) * celsius + 32).toFixed(1)
 };
 
 // Do not edit below this line
