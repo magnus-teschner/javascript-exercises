b003025aa9e8e9b3c9ec8b327be24e415da92915
Magnus Teschner <magnus.2002.mt@gmail.com>
2023-11-18 16:05:19 +0100

    Finish exercise 2!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,9 @@
 const repeatString = function(word, amount) {
     let resultString = '';
-    if (!amount || amount <= 0){
+    if (!amount || amount === 0){
         return ''
+    } else if (amount < 0){
+        return 'ERROR'
     }
 
     for (let i = 0; i < amount; i++){
