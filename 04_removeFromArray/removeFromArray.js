8584166cbf308612a398f340202a938c1b90ebfc
Magnus Teschner <magnus.2002.mt@gmail.com>
2023-11-18 16:29:57 +0100

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeValues) {
+    const toRemove = [];
+
+    removeValues.forEach((remove) =>{
+        for (let i = 0; i < arr.length; i++){
+            if (arr[i] === remove){
+                toRemove.push(i);
+            }
+        }
+    })
+
+    
+
+    toRemove.forEach((i) => {
+        delete arr[i]
+    })
+
+    const new_arr = arr.filter(x => x !== undefined)
+
+    return new_arr
+
 
 };
 
