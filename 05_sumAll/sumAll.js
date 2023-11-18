10e4e87b9d579c13fb99ed6032181a2637934600
Magnus Teschner <magnus.2002.mt@gmail.com>
2023-11-18 16:48:20 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let totalSum = 0;
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number'){
+        return 'ERROR'
+    } else if (num1 > num2){
+        for (let i = num2; i <= num1; i++){
+            totalSum += i
+        }
+    } else if (num2 > num1){
+        for (let i = num1; i <= num2; i++){
+            totalSum += i
+        }
+    } else {
+        totalSum = 2*num1
+    }
+
+    return totalSum
+    
 
 };
 
